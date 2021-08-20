c686dc1ea14c787fafbfc691d36b18ed16b301c8
Tyler Kanz <tylerkanz@gmail.com>
2021-08-20 13:56:58 -0400

    New Button style

--- inc/accessories.php
+++ inc/accessories.php
@@ -5,12 +5,22 @@ add_shortcode('tk_button', 'tk_button_func');
 function tk_button_func($atts = array())
 {
     extract(shortcode_atts(array('text' => '', 'href' => '#', 'type' => 'text'), $atts)); ?>
-    <a href="<?php echo $href; ?>" class="btn btn-outline-primary">
-        <?php if ($type == 'icon') : ?>
-            <i class="<?php echo $text; ?>"></i>
-        <?php endif; ?>
-        <?php if ($type == 'text') : ?>
-            <?php echo $text; ?>
-        <?php endif; ?>
-    </a>
+    <div class="portfolio-experiment">
+        <a href="<?php echo $href; ?>">
+            <?php if ($type == 'icon') : ?>
+                <span class="text"><i class="<?php echo $text; ?>"></i></span>
+                <span class="line -right"></span>
+                <span class="line -top"></span>
+                <span class="line -left"></span>
+                <span class="line -bottom"></span>
+            <?php endif; ?>
+            <?php if ($type == 'text') : ?>
+                <span class="text"><?php echo $text; ?></span>
+                <span class="line -right"></span>
+                <span class="line -top"></span>
+                <span class="line -left"></span>
+                <span class="line -bottom"></span>
+            <?php endif; ?>
+        </a>
+    </div>
 <?php } ?>
